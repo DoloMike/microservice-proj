a01bc81f4edd75a48fde0107f4450d0feaba1e5d
Michael Roberts <DoloMike@users.noreply.github.com>
2018-11-20 12:01:06 -0600

    add heroku endpoints to environment.prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+	production: true,
+	loansApiEndpoint: 'https://stormy-brushlands-69349.herokuapp.com/',
+	customersApiEndpoint: 'https://stark-brook-17892.herokuapp.com/'
 };
